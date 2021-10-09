8a6147d57c1ffb9fc69fe23dc461319c2b20c95c
Jorge De Jesus Rivera Perez <i20163mc061@utez.edu.mx>
2021-10-08 23:16:25 -0500

    Punto de muerte
    v1

--- js/search_cash.js
+++ js/search_cash.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
-    var busqueda = $('#busqueda'),
-    titulo = $('table tbody tr td a');
+    var busqueda = $('#busqueda');
+    var titulo = $('table tbody tr td a');
 
     $(titulo).each(function(){
         var li = $(this);
@@ -13,13 +13,16 @@ $(document).ready(function(){
         var clase = $('.search svg');
         if($(busqueda).val() != ''){
         $(clase).attr('class', 'fas fa-times');
+        $(".content-search").show();
         }else{
         $(clase).attr('class', 'fas fa-search');
+        $(".content-search").hide();
+
         }
         if($(clase).hasClass('fas fa-times')){
         $(clase).click(function(){
         //borramos el contenido del input
-        $(busqueda).val(" ");
+        $(busqueda).val('');
         //mostramos todas las listas
         $(li).parent().show();
         //volvemos a añadir la clase para mostrar la lupa
@@ -34,12 +37,16 @@ $(document).ready(function(){
         if($(li).text().toLowerCase().indexOf(txt) > -1){
         //mostramos las listas que coincidan
         $(li).parent().show();
+        var resultado = $(li).parent();
+        var resultado2 = resultado.text();
         }
         });
         });    
 
+        $(titulo).click(function(){
+            console.log($(this.attr('a')));
+        })
 
 });
    
-
     
